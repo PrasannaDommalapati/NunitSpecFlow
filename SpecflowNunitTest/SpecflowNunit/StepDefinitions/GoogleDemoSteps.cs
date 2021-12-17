c6d0e455d977832a9aade6ab37d1a152ee479982
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-12-17 23:33:22 +0000

    initial commit

---
+++ SpecflowNunitTest/SpecflowNunit/StepDefinitions/GoogleDemoSteps.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowNunit.StepDefinitions
+{
+    [Binding]
+    public class GoogleDemoSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly IWebDriver _driver;
+
+        public GoogleDemoSteps(ScenarioContext scenarioContext, IWebDriver driver)
+        {
+            _scenarioContext = scenarioContext;
+            _driver = driver;
+        }
+
+        [Given(@"I am in ""(.*)""")]
+        public void GivenIAmIn(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+        
+        [When(@"I enter a keyword ""(.*)""")]
+        public void WhenIEnterAKeyword(string keyword)
+        {
+            _driver.SwitchTo().ActiveElement();
+            _driver.FindElement(By.Id("L2AGLb")).Click();
+            _driver.FindElement(By.Name("q")).SendKeys(keyword);
+            _driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+        }
+        
+        [Then(@"I should see the page title contains ""(.*)""")]
+        public void ThenIShouldSeeThePageTitleContains(string titleText)
+        {
+            _driver.Title.Should().StartWith(titleText);
+        }
+    }
+}
---
+++ SpecflowNunitTest/SpecflowNunit/StepDefinitions/Hooks.cs
@@ -0,0 +1,63 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace SpecflowNunit.StepDefinitions
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        private IObjectContainer _objectContainer { get; set; }
+        private IWebDriver _driver { get; set; }
+        private readonly ScenarioContext _scenarioContext;
+        private static Helpers.ExtentReportsHelper _extentHelper;
+
+        public Hooks(IObjectContainer objectContainer, ScenarioContext context)
+        {
+            _scenarioContext = context;
+            _objectContainer = objectContainer;
+            _extentHelper = _extentHelper = new Helpers.ExtentReportsHelper();
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _extentHelper.CreateTest(_scenarioContext.ScenarioInfo.Title);
+            _driver = new ChromeDriver();
+            _objectContainer.RegisterInstanceAs<IWebDriver>(_driver);
+        }
+
+        [AfterScenario]
+        public void WrapUpReport()
+        {
+            switch (_scenarioContext.ScenarioExecutionStatus)
+            {
+                case ScenarioExecutionStatus.OK:
+                    _extentHelper.SetTestStatusPass();
+                    break;
+                case ScenarioExecutionStatus.TestError:
+                    _extentHelper.SetTestStatusFail("One or more errors occurred during scenario execution");
+                    break;
+                case ScenarioExecutionStatus.UndefinedStep:
+                    _extentHelper.SetStepStatusWarning(_scenarioContext.ScenarioInfo.Title);
+                    break;
+                case ScenarioExecutionStatus.Skipped:
+                    _extentHelper.SetTestStatusSkipped();
+                    break;
+                default:
+                    _extentHelper.SetTestStatusFail("Error occurred during determination of scenario status");
+                    break;
+            }
+
+            _scenarioContext.Clear();
+            _driver.Quit();
+        }
+
+        [AfterFeature]
+        public static void AfterFeature()
+        {
+            _extentHelper.Close();
+        }
+    }
+}
