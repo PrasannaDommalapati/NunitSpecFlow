a720353911552eca85b7ceb397590cc2e57c93ef
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-12-18 13:14:50 +0000

    enhanced extent reporting

--- SpecflowNunitTest/SpecflowNunit/StepDefinitions/Hooks.cs
+++ SpecflowNunitTest/SpecflowNunit/StepDefinitions/Hooks.cs
@@ -1,63 +1,109 @@
-﻿using BoDi;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using BoDi;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 using TechTalk.SpecFlow;
 
 namespace SpecflowNunit.StepDefinitions
 {
     [Binding]
-    public sealed class Hooks
+    public class Hooks : Steps
     {
+        private static ExtentTest _featureName;
+        private static ExtentTest _scnarioName;
+        private static ExtentReports extent;
         private IObjectContainer _objectContainer { get; set; }
         private IWebDriver _driver { get; set; }
         private readonly ScenarioContext _scenarioContext;
-        private static Helpers.ExtentReportsHelper _extentHelper;
+        private readonly FeatureContext _featureContext;
 
-        public Hooks(IObjectContainer objectContainer, ScenarioContext context)
+        public Hooks(IObjectContainer objectContainer, ScenarioContext context, FeatureContext featureContext)
         {
-            _scenarioContext = context;
             _objectContainer = objectContainer;
-            _extentHelper = _extentHelper = new Helpers.ExtentReportsHelper();
+            _scenarioContext = context;
+            _featureContext = featureContext;
+        }
+
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            var htmlReporter = new ExtentHtmlReporter(@"C:\Users\Prasanna Dommalapati\Desktop\Reports\ExtentReports.html");
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
         }
 
         [BeforeScenario]
         public void BeforeScenario()
         {
-            _extentHelper.CreateTest(_scenarioContext.ScenarioInfo.Title);
             _driver = new ChromeDriver();
             _objectContainer.RegisterInstanceAs<IWebDriver>(_driver);
+            _scnarioName = _featureName.CreateNode<Scenario>(_scenarioContext.ScenarioInfo.Title);
+        }
+
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            extent.Flush();
+        }
+
+        [AfterStep]
+        public void InsertReportingSteps(ScenarioContext scenarioContext)
+        {
+            var stepType = scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+            PropertyInfo pInfo = typeof(ScenarioContext).GetProperty("ScenarioExecutionStatus", BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo getter = pInfo.GetGetMethod(nonPublic: true);
+            object TestResult = getter.Invoke(_scenarioContext, null);
+            if (scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                    _scnarioName.CreateNode<Given>(scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "When")
+                    _scnarioName.CreateNode<When>(scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "Then")
+                    _scnarioName.CreateNode<Then>(scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "And")
+                    _scnarioName.CreateNode<And>(scenarioContext.StepContext.StepInfo.Text);
+            }
+
+            if (scenarioContext.TestError != null)
+            {
+                if (stepType == "Given")
+                {
+                    _scnarioName.CreateNode<Given>(scenarioContext.StepContext.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                    _scnarioName.AddScreenCaptureFromBase64String(_driver.ScreenCaptureAsBase64String());
+                }
+                else if (stepType == "When")
+                {
+                    _scnarioName.CreateNode<When>(scenarioContext.StepContext.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                    _scnarioName.AddScreenCaptureFromBase64String(_driver.ScreenCaptureAsBase64String());
+                }
+                else if (stepType == "Then")
+                {
+                    _scnarioName.CreateNode<Then>(scenarioContext.StepContext.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                    _scnarioName.AddScreenCaptureFromBase64String(_driver.ScreenCaptureAsBase64String());
+                }
+                else if (stepType == "And")
+                {
+                    _scnarioName.CreateNode<And>(scenarioContext.StepContext.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                    _scnarioName.AddScreenCaptureFromBase64String(_driver.ScreenCaptureAsBase64String());
+                }
+            }
         }
 
         [AfterScenario]
-        public void WrapUpReport()
+        public void AfterScenario()
         {
-            switch (_scenarioContext.ScenarioExecutionStatus)
-            {
-                case ScenarioExecutionStatus.OK:
-                    _extentHelper.SetTestStatusPass();
-                    break;
-                case ScenarioExecutionStatus.TestError:
-                    _extentHelper.SetTestStatusFail("One or more errors occurred during scenario execution");
-                    break;
-                case ScenarioExecutionStatus.UndefinedStep:
-                    _extentHelper.SetStepStatusWarning(_scenarioContext.ScenarioInfo.Title);
-                    break;
-                case ScenarioExecutionStatus.Skipped:
-                    _extentHelper.SetTestStatusSkipped();
-                    break;
-                default:
-                    _extentHelper.SetTestStatusFail("Error occurred during determination of scenario status");
-                    break;
-            }
-
-            _scenarioContext.Clear();
-            _driver.Quit();
+            _driver.Close();
         }
 
-        [AfterFeature]
-        public static void AfterFeature()
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
         {
-            _extentHelper.Close();
+            _featureName = extent.CreateTest(featureContext.FeatureInfo.Title);
         }
     }
 }
